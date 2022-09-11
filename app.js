0b1cf760ecb2c4cb7fb0f204f31d1cdc3f4bfe2e
amalworks <amal.play@outlook.com>
2022-09-11 12:16:00 -0700

    added functionality to top-bar

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+//lesson directory code
 let lessonButtonClicked = false;
 document.getElementById('lessons-button').onclick = function(){
     if(lessonButtonClicked === false) {
         document.getElementById('lessons-button').style.borderBottomLeftRadius = '0';
         document.getElementById('lessons-button').style.borderBottomRightRadius = '0';
-        document.getElementById('startup-directory').style.height = '370px';
+        document.getElementById('startup-directory').style.height = '425px';
         document.getElementById('lcb').style.opacity = '100%';
         document.getElementById('lcb').style.scale = '1';
         document.getElementById('lcb').style.marginTop = '-10px';
@@ -14,7 +15,7 @@ document.getElementById('lessons-button').onclick = function(){
     } else if(lessonButtonClicked === true) {
         document.getElementById('lessons-button').style.borderBottomLeftRadius = '20px';
         document.getElementById('lessons-button').style.borderBottomRightRadius = '20px';
-        document.getElementById('startup-directory').style.height = '85px';
+        document.getElementById('startup-directory').style.height = '140px';
         document.getElementById('lcb').style.opacity = '0';
         document.getElementById('lcb').style.scale = '0';
         document.getElementById('lcb').style.marginTop = '0';
@@ -24,7 +25,29 @@ document.getElementById('lessons-button').onclick = function(){
         lessonButtonClicked = false;
     }
 }
+//  UNIVERSAL CODE
+
+let previousSlide = document.getElementById('startup-screen');
+
+//Credits button/code section
 document.getElementById('credit-button').onclick = function(){
     document.getElementById('startup-screen').classList.replace('active', 'inactive');
     document.getElementById('cred-sec').classList.replace('inactive', 'active');
+}
+
+let credTopToggled = false;
+document.getElementById('cred-top-bar-toggle').onclick = function(){
+    if(credTopToggled === false) {
+        document.getElementById('cred-top-bar').style.marginTop = '-70px';
+        document.getElementById('cred-top-bar-text').style.rotate = '180deg';
+        credTopToggled = true;
+    } else if(credTopToggled === true){
+        document.getElementById('cred-top-bar').style.marginTop = '0';
+        document.getElementById('cred-top-bar-text').style.rotate = '360deg';
+        credTopToggled = false;
+    }
+}
+document.getElementById('cred-return-button').onclick = function(){
+    previousSlide.classList.replace('inactive', 'active');
+    document.getElementById('cred-sec').classList.replace('active', 'inactive');
 }
